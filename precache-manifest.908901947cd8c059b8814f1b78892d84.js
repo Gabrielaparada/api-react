3b0f47c03552e65fce52eb0516c72812ca790e62
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-05-02 12:06:07 -0400

    Updates

---
+++ precache-manifest.908901947cd8c059b8814f1b78892d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435a01015f18c79e9abf8c17eff2827f",
+    "url": "/api-react/index.html"
+  },
+  {
+    "revision": "39a23b668af795bda25d",
+    "url": "/api-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2288b471d5b6c92a046",
+    "url": "/api-react/static/js/2.b58495b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-react/static/js/2.b58495b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a23b668af795bda25d",
+    "url": "/api-react/static/js/main.88439645.chunk.js"
+  },
+  {
+    "revision": "8123d3dd4c4ce87ecd59",
+    "url": "/api-react/static/js/runtime-main.43c99607.js"
+  }
+]);
